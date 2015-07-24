d08a52733dc8e65e459abb5b861fa26e207d1d66
Björn Zapadlo <bjoern.zapadlo@holidaycheck.com>
2015-07-24 18:24:38 +0200

    Working on the time

--- javascript/Qlock.js
+++ javascript/Qlock.js
@@ -7,7 +7,7 @@ define(['ClassNames', 'react'], function (ClassNames, React) {
     var QlockBlock = React.createClass({
         refreshClock: function() {
             var qlock = new Qlock(new Date());
-            this.setState({time: time, minAndSec: qlock.getMinAndSec()});
+            this.setState({time: qlock.getTime(), minAndSec: qlock.getMinAndSec()});
         },
         getInitialState: function() {
             return {time: [], minAndSec:[]};
@@ -51,139 +51,6 @@ define(['ClassNames', 'react'], function (ClassNames, React) {
             );
         }
     });
-
-    var time = [
-        [
-            {char: "E", active: true},
-            {char: "S", active: true},
-            {char: "M", active: false},
-            {char: "I", active: true},
-            {char: "S", active: true},
-            {char: "T", active: true},
-            {char: "E", active: false},
-            {char: "F", active: true},
-            {char: "Ü", active: true},
-            {char: "N", active: true},
-            {char: "F", active: true}
-        ],
-        [
-            {char: "Z", active: false},
-            {char: "E", active: false},
-            {char: "H", active: false},
-            {char: "N", active: false},
-            {char: "Z", active: false},
-            {char: "W", active: false},
-            {char: "A", active: false},
-            {char: "N", active: false},
-            {char: "Z", active: false},
-            {char: "I", active: false},
-            {char: "G", active: false}
-        ],
-        [
-            {char: "N", active: true},
-            {char: "A", active: true},
-            {char: "C", active: true},
-            {char: "H", active: true},
-            {char: "V", active: false},
-            {char: "I", active: false},
-            {char: "E", active: false},
-            {char: "R", active: false},
-            {char: "T", active: false},
-            {char: "E", active: false},
-            {char: "L", active: false}
-        ],
-        [
-            {char: "V", active: false},
-            {char: "O", active: false},
-            {char: "R", active: false},
-            {char: "N", active: false},
-            {char: "A", active: false},
-            {char: "C", active: false},
-            {char: "H", active: false},
-            {char: "H", active: false},
-            {char: "A", active: false},
-            {char: "L", active: false},
-            {char: "B", active: false}
-        ],
-        [
-            {char: "E", active: false},
-            {char: "I", active: false},
-            {char: "N", active: false},
-            {char: "S", active: false},
-            {char: "I", active: false},
-            {char: "N", active: false},
-            {char: "K", active: false},
-            {char: "Z", active: false},
-            {char: "W", active: false},
-            {char: "E", active: false},
-            {char: "I", active: false}
-        ],
-        [
-            {char: "D", active: false},
-            {char: "R", active: false},
-            {char: "E", active: false},
-            {char: "I", active: false},
-            {char: "E", active: false},
-            {char: "A", active: false},
-            {char: "N", active: false},
-            {char: "V", active: false},
-            {char: "I", active: false},
-            {char: "E", active: false},
-            {char: "R", active: false}
-        ],
-        [
-            {char: "F", active: false},
-            {char: "Ü", active: false},
-            {char: "N", active: false},
-            {char: "F", active: false},
-            {char: "N", active: false},
-            {char: "I", active: false},
-            {char: "S", active: false},
-            {char: "E", active: false},
-            {char: "C", active: false},
-            {char: "H", active: false},
-            {char: "S", active: false}
-        ],
-        [
-            {char: "S", active: false},
-            {char: "I", active: false},
-            {char: "E", active: false},
-            {char: "B", active: false},
-            {char: "E", active: false},
-            {char: "N", active: false},
-            {char: "I", active: false},
-            {char: "A", active: false},
-            {char: "C", active: false},
-            {char: "H", active: false},
-            {char: "T", active: false}
-        ],
-        [
-            {char: "N", active: false},
-            {char: "E", active: false},
-            {char: "U", active: false},
-            {char: "N", active: false},
-            {char: "Z", active: false},
-            {char: "E", active: false},
-            {char: "H", active: false},
-            {char: "N", active: false},
-            {char: "E", active: false},
-            {char: "L", active: false},
-            {char: "F", active: false}
-        ],
-        [
-            {char: "Z", active: true},
-            {char: "W", active: true},
-            {char: "Ö", active: true},
-            {char: "L", active: true},
-            {char: "F", active: true},
-            {char: "K", active: false},
-            {char: "A", active: false},
-            {char: "B", active: false},
-            {char: "U", active: false},
-            {char: "H", active: false},
-            {char: "R", active: false}
-        ]
-    ];
 
     React.render(
         <QlockBlock refreshIntervall={1000} />,
@@ -238,16 +105,79 @@ class QlockCalculatorSeconds extends QlockCalculatorMinutesAndSeconds {
     };
 }
 
+class QlockCalculatorTime {
+    constructor(date) {
+
+        this.layout = [
+            ["E", "S", "M", "I", "S", "T", "E", "F", "Ü", "N", "F"],
+            ["Z", "E", "H", "N", "Z", "W", "A", "N", "Z", "I", "G"],
+            ["N", "A", "C", "H", "V", "I", "E", "R", "T", "E", "L"],
+            ["V", "O", "R", "N", "A", "C", "H", "H", "A", "L", "B"],
+            ["E", "I", "N", "S", "I", "N", "K", "Z", "W", "E", "I"],
+            ["D", "R", "E", "I", "E", "A", "N", "V", "I", "E", "R"],
+            ["F", "Ü", "N", "F", "N", "I", "S", "E", "C", "H", "S"],
+            ["S", "I", "E", "B", "E", "N", "I", "A", "C", "H", "T"],
+            ["N", "E", "U", "N", "Z", "E", "H", "N", "E", "L", "F"],
+            ["Z", "W", "Ö", "L", "F", "K", "A", "B", "U", "H", "R"]
+        ];
+
+        this.staticMap = [[0, 0], [0, 1], [0, 3], [0, 4], [0, 5]];
+
+        this.minutesMap = [
+            {
+                match: [5, 6, 7, 8, 9],
+                active: [[0, 7], [0, 8], [0, 9], [0, 10]]
+            },
+        ]
+    }
+
+    isActiveMinute(x, y) {
+        return false;
+    }
+
+    isActiveHour(x, y) {
+        return false;
+    }
+
+    isActiveStatic(x, y) {
+        var returnValue = false;
+
+        this.staticMap.forEach(function(elem) {
+            if(elem[0] == x && elem[1] == y) {
+                returnValue = true;
+                return;
+            }
+        });
+
+        return returnValue;
+    }
+
+    isActive(x, y) {
+        return this.isActiveStatic(x, y) || this.isActiveMinute(x, y) || this.isActiveHour(x, y); // check against static map
+    }
+
+    get() {
+        return this.layout.map(
+            function(row, rowIndex) {
+                return row.map(function(char, columIndex) {
+                    return QlockCellDecorator.decorate(char, this.isActive(rowIndex, columIndex));
+                }.bind(this));
+            }.bind(this)
+        );
+    }
+}
+
 
 class Qlock {
     constructor(date) {
         this.minutesCalculator = new QlockCalculatorMinutes(date);
         this.secondsCalculator = new QlockCalculatorSeconds(date);
+        this.timeCalculator = new QlockCalculatorTime(date);
 
     }
 
     getTime() {
-
+        return this.timeCalculator.get();
     }
 
     getMinAndSec() {
